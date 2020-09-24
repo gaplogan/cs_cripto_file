e742dac2ffeb50bb56c5b508f3878f77ba6731bd
Gabriel Artigas <gaplogan@gmail.com>
2020-09-24 18:39:11 -0300

    2020.09.24.18.39

---
+++ CriptoFile/CriptoFile/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace CriptoFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonEncryptFile = new System.Windows.Forms.Button();
+            this.buttonDecryptFile = new System.Windows.Forms.Button();
+            this.buttonCreateAsmKeys = new System.Windows.Forms.Button();
+            this.buttonExportPublicKey = new System.Windows.Forms.Button();
+            this.buttonImportPublicKey = new System.Windows.Forms.Button();
+            this.buttonGetPrivateKey = new System.Windows.Forms.Button();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(762, 104);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chave não definida";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonEncryptFile
+            // 
+            this.buttonEncryptFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEncryptFile.Location = new System.Drawing.Point(12, 200);
+            this.buttonEncryptFile.Name = "buttonEncryptFile";
+            this.buttonEncryptFile.Size = new System.Drawing.Size(250, 75);
+            this.buttonEncryptFile.TabIndex = 1;
+            this.buttonEncryptFile.Text = "Criptografar arquivo";
+            this.buttonEncryptFile.UseVisualStyleBackColor = true;
+            this.buttonEncryptFile.Click += new System.EventHandler(this.buttonEncryptFile_Click);
+            // 
+            // buttonDecryptFile
+            // 
+            this.buttonDecryptFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDecryptFile.Location = new System.Drawing.Point(268, 200);
+            this.buttonDecryptFile.Name = "buttonDecryptFile";
+            this.buttonDecryptFile.Size = new System.Drawing.Size(250, 75);
+            this.buttonDecryptFile.TabIndex = 2;
+            this.buttonDecryptFile.Text = "Descriptografar arquivo";
+            this.buttonDecryptFile.UseVisualStyleBackColor = true;
+            this.buttonDecryptFile.Click += new System.EventHandler(this.buttonDecryptFile_Click);
+            // 
+            // buttonCreateAsmKeys
+            // 
+            this.buttonCreateAsmKeys.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCreateAsmKeys.Location = new System.Drawing.Point(524, 200);
+            this.buttonCreateAsmKeys.Name = "buttonCreateAsmKeys";
+            this.buttonCreateAsmKeys.Size = new System.Drawing.Size(250, 75);
+            this.buttonCreateAsmKeys.TabIndex = 3;
+            this.buttonCreateAsmKeys.Text = "Criar chaves";
+            this.buttonCreateAsmKeys.UseVisualStyleBackColor = true;
+            this.buttonCreateAsmKeys.Click += new System.EventHandler(this.buttonCreateAsmKeys_Click);
+            // 
+            // buttonExportPublicKey
+            // 
+            this.buttonExportPublicKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExportPublicKey.Location = new System.Drawing.Point(12, 281);
+            this.buttonExportPublicKey.Name = "buttonExportPublicKey";
+            this.buttonExportPublicKey.Size = new System.Drawing.Size(250, 75);
+            this.buttonExportPublicKey.TabIndex = 4;
+            this.buttonExportPublicKey.Text = "Exportar chave pública";
+            this.buttonExportPublicKey.UseVisualStyleBackColor = true;
+            this.buttonExportPublicKey.Click += new System.EventHandler(this.buttonExportPublicKey_Click);
+            // 
+            // buttonImportPublicKey
+            // 
+            this.buttonImportPublicKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonImportPublicKey.Location = new System.Drawing.Point(268, 281);
+            this.buttonImportPublicKey.Name = "buttonImportPublicKey";
+            this.buttonImportPublicKey.Size = new System.Drawing.Size(250, 75);
+            this.buttonImportPublicKey.TabIndex = 5;
+            this.buttonImportPublicKey.Text = "Importar chave pública";
+            this.buttonImportPublicKey.UseVisualStyleBackColor = true;
+            this.buttonImportPublicKey.Click += new System.EventHandler(this.buttonImportPublicKey_Click);
+            // 
+            // buttonGetPrivateKey
+            // 
+            this.buttonGetPrivateKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGetPrivateKey.Location = new System.Drawing.Point(524, 281);
+            this.buttonGetPrivateKey.Name = "buttonGetPrivateKey";
+            this.buttonGetPrivateKey.Size = new System.Drawing.Size(250, 75);
+            this.buttonGetPrivateKey.TabIndex = 6;
+            this.buttonGetPrivateKey.Text = "Obter chave privada";
+            this.buttonGetPrivateKey.UseVisualStyleBackColor = true;
+            this.buttonGetPrivateKey.Click += new System.EventHandler(this.buttonGetPrivateKey_Click);
+            // 
+            // txtKey
+            // 
+            this.txtKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKey.Location = new System.Drawing.Point(268, 162);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(250, 32);
+            this.txtKey.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 370);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.buttonGetPrivateKey);
+            this.Controls.Add(this.buttonImportPublicKey);
+            this.Controls.Add(this.buttonExportPublicKey);
+            this.Controls.Add(this.buttonCreateAsmKeys);
+            this.Controls.Add(this.buttonDecryptFile);
+            this.Controls.Add(this.buttonEncryptFile);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonEncryptFile;
+        private System.Windows.Forms.Button buttonDecryptFile;
+        private System.Windows.Forms.Button buttonCreateAsmKeys;
+        private System.Windows.Forms.Button buttonExportPublicKey;
+        private System.Windows.Forms.Button buttonImportPublicKey;
+        private System.Windows.Forms.Button buttonGetPrivateKey;
+        private System.Windows.Forms.TextBox txtKey;
+    }
+}
+
---
+++ CriptoFile/CriptoFile/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace CriptoFile
+{
+    public partial class Form1 : Form
+    {        
+        public Form1()
+        {
+            InitializeComponent();
+            Criptografia.cspp = new CspParameters();
+            Criptografia.EncrFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Encrypt\";
+            Criptografia.DecrFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Decrypt\";
+            Criptografia.SrcFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        }
+
+        /// <summary>
+        /// Criptografando um arquivo
+        /// Essa tarefa envolve dois métodos: o método manipulador de eventos para o 
+        /// Encrypt File botão(buttonEncryptFile_Click ) e o EncryptFile método.
+        /// O primeiro método exibe uma caixa de diálogo para selecionar um arquivo e 
+        /// passa o nome do arquivo para o segundo método, que executa a criptografia.
+        /// O conteúdo criptografado, a chave e o IV são todos salvos em um FileStream,
+        /// que é conhecido como o pacote de criptografia.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonEncryptFile_Click(object sender, EventArgs e)
+        {
+            if (Criptografia.rsa == null)
+            {
+                label1.ForeColor = Color.Red;
+                label1.Text = "Chave não definida.";
+            }
+            else
+            {
+                // Mostra uma caixa de dialogo para escolher um arquivo para criptografar.
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.InitialDirectory = Criptografia.SrcFolder;
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    string fName = dialog.FileName;
+                    if (fName != null)
+                    {
+                        FileInfo fInfo = new FileInfo(fName);
+                        // Passsa o nome do arquivo sem o caminho.
+                        string name = fInfo.FullName;
+                        label1.Text = Criptografia.EncryptFile(name);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Descriptografando um arquivo
+        /// Essa tarefa envolve dois métodos, o método manipulador de eventos para o 
+        /// Decrypt File botão(buttonDecryptFile_Click ) e o DecryptFile método.
+        /// O primeiro método exibe uma caixa de diálogo para selecionar um arquivo e 
+        /// passa seu nome de arquivo para o segundo método, que executa a descriptografia.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonDecryptFile_Click(object sender, EventArgs e)
+        {
+            if (Criptografia.rsa == null)
+            {
+                label1.ForeColor = Color.Red;
+                label1.Text = "Chave não definida.";
+            }
+            else
+            {
+                // Mostra uma caixa de dialogo para escolher um arquivo para descriptografar.
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.InitialDirectory = Criptografia.EncrFolder;
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    string fName = dialog.FileName;
+                    if (fName != null)
+                    {
+                        FileInfo fi = new FileInfo(fName);
+                        string name = fi.Name;
+                        label1.Text = Criptografia.DecryptFile(name);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Criando uma chave assimétrica:
+        /// Essa tarefa cria uma chave assimétrica que criptografa e descriptografa a chave Aes.
+        /// Essa chave foi usada para criptografar o conteúdo e exibe o nome do contêiner de chave no controle rótulo.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCreateAsmKeys_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtKey.Text))
+            {
+                label1.ForeColor = Color.Red;
+                label1.Text = "Insira um valor para definir a chave pública.";
+                txtKey.Focus();
+                return;
+            }
+
+            Criptografia.keyName = txtKey.Text;
+            label1.ForeColor = Color.DarkBlue;
+            label1.Text = Criptografia.CreateAsmKeys();             
+        }
+
+        /// <summary>
+        /// Exportando uma chave pública
+        /// Essa tarefa salva a chave criada pelo Create Keys botão em um arquivo.Ele exporta apenas os parâmetros públicos.
+        /// Essa tarefa simula o cenário de Alice dando a Bob sua chave pública para que ele possa criptografar arquivos. 
+        /// Ele e outros que tenham essa chave pública não poderão descriptografá-los 
+        /// porque não têm o par de chaves completo com parâmetros privados.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonExportPublicKey_Click(object sender, EventArgs e)
+        {
+            if (Criptografia.ExportPublicKey())
+            {
+                label1.ForeColor = Color.DarkBlue;
+                label1.Text = "Chave publica exportada";
+            }
+            else
+            {
+                label1.ForeColor = Color.Red;
+                label1.Text = "Chave publica não exportada";
+            }
+        }
+
+        /// <summary>
+        /// Importando uma chave pública
+        /// Essa tarefa carrega a chave somente com parâmetros públicos, conforme criado pelo 
+        /// Export Public Key botão e o define como o nome do contêiner de chave.
+        /// Esta tarefa simula o cenário de Bob carregando a chave de Alice com apenas parâmetros públicos, 
+        /// portanto, ele pode criptografar arquivos.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonImportPublicKey_Click(object sender, EventArgs e)
+        {
+            //if (string.IsNullOrEmpty(txtKey.Text))
+            //{
+            //    label1.ForeColor = Color.Red;
+            //    label1.Text = "Insira um valor para definir a chave pública.";
+            //    txtKey.Focus();
+            //    return;
+            //}
+
+            Criptografia.keyName = "Importada";
+            label1.ForeColor = Color.DarkBlue;
+            label1.Text = Criptografia.ImportPublicKey();
+        }
+
+        /// <summary>
+        /// Obtendo uma chave privada
+        /// Essa tarefa define o nome do contêiner de chave como o nome da chave criada usando o Create Keys botão.
+        /// O contêiner de chave conterá o par de chaves completo com parâmetros privados.
+        /// Esta tarefa simula o cenário de Alice usando sua chave privada para descriptografar arquivos criptografados por Bob.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonGetPrivateKey_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtKey.Text))
+            {
+                label1.ForeColor = Color.Red;
+                label1.Text = "Insira um valor para definir a chave privada.";
+                txtKey.Focus();
+                return;
+            }
+
+            Criptografia.keyName = txtKey.Text;
+            label1.ForeColor = Color.DarkBlue;
+            label1.Text = Criptografia.GetPrivateKey();
+        }
+    }
+}
